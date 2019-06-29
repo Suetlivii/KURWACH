74605e06f70a11fa029b83f810a078f26200788c
Suetlivii <sueta@mail.ru>
2019-06-30 02:03:25 +0300

    GAW1991: A2A and A2G bots added and tested

---
+++ Georgia-Abkhazia War 1991/A2AConfigGAW1991.lua
@@ -0,0 +1,53 @@
+-- ["Gelendzhik"] = "Gelendzhik",
+-- ["Krasnodar_Pashkovsky"] = "Krasnodar-Pashkovsky",
+-- ["Sukhumi_Babushara"] = "Sukhumi-Babushara",
+-- ["Gudauta"] = "Gudauta",
+-- ["Batumi"] = "Batumi",
+-- ["Senaki_Kolkhi"] = "Senaki-Kolkhi",
+-- ["Kobuleti"] = "Kobuleti",
+-- ["Kutaisi"] = "Kutaisi",
+-- ["Tbilisi_Lochini"] = "Tbilisi-Lochini",
+-- ["Soganlug"] = "Soganlug",
+-- ["Vaziani"] = "Vaziani",
+-- ["Anapa_Vityazevo"] = "Anapa-Vityazevo",
+-- ["Krasnodar_Center"] = "Krasnodar-Center",
+-- ["Novorossiysk"] = "Novorossiysk",
+-- ["Krymsk"] = "Krymsk",
+-- ["Maykop_Khanskaya"] = "Maykop-Khanskaya",
+-- ["Sochi_Adler"] = "Sochi-Adler",
+-- ["Mineralnye_Vody"] = "Mineralnye Vody",
+-- ["Nalchik"] = "Nalchik",
+-- ["Mozdok"] = "Mozdok",
+-- ["Beslan"] = "Beslan",
+
+A2AConfig = 
+{
+    {
+        groupPrefix = "capMig29",
+        spawnedGroupPrefix = "CapMig29S",
+        minAltitude = 3000,
+        maxAltitude = 6000,
+        airBase = "Gudauta",
+        isCap = true,
+        isGci = false,
+        grouping = 2,
+        overhead = 1,
+        airCraftLimit = 8,
+        minFrontlineDistance = 40000,
+        maxFrontlineDistance = 100000
+    },
+    {
+        groupPrefix = "BlueGeorgianCAPF16",
+        spawnedGroupPrefix = "GeorgianCAPF16",
+        minAltitude = 3000,
+        maxAltitude = 6000,
+        airBase = "Soganlug",
+        isCap = true,
+        isGci = false,
+        grouping = 2,
+        overhead = 1,
+        airCraftLimit = 8,
+        minFrontlineDistance = 40000,
+        maxFrontlineDistance = 100000
+    }
+}
---
+++ Georgia-Abkhazia War 1991/MainGAW1991.lua
@@ -0,0 +1,40 @@
+-----------------------------------------------------------------------------------------------------------------------------------------------
+-- Debuger used for debug
+-- 
+-----------------------------------------------------------------------------------------------------------------------------------------------
+Debuger = {}
+
+function Debuger:New(_isDebugMode)
+    newObj = 
+    {
+        isDebugMode = _isDebugMode
+    }
+    self.__index = self
+    return setmetatable(newObj, self)
+end
+
+function Debuger:Log(msgString)
+    if self.isDebugMode == true then
+        newDebugMessage = MESSAGE:New(tostring(msgString .. "\n"), 10, "DEBUG", false):ToAll()
+        BASE:E(tostring(msgString))
+    end
+end
+
+-----------------------------------------------------------------------------------------------------------------------------------------------
+
+Debug = Debuger:New(true)
+
+local redZone = ZONE:FindByName("RedZone")
+local blueZone = ZONE:FindByName("BlueZone")
+
+mainA2AController = A2AController:New()
+local frontZone = ZONE:FindByName("FrontZone")
+mainA2AController:SetDispatcher("EWR", 230000, 80000, 135000, frontZone, redZone, blueZone)
+
+mainA2AConfigurator = A2AConfigurator:New()
+mainA2AConfigurator:SetA2AConfigs(A2AConfig)
+mainA2AConfigurator:SetController(mainA2AController)
+
+mainA2GController = A2GController:New(A2GConfigs)
+mainA2GController:SetZones(blueZone, redZone, blueZone)
+mainA2GController:StartBAI(1800, 0.5, 1)
